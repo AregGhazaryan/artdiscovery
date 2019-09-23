47feb99379823b46b071687017dd2c9ccf9e813c
Karen Stepanyan <kstepananyan@gmail.com>
2019-09-23 17:58:25 +0400

    svg animation

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,72 +1,146 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-          <div class="logo-pieces">
+  <div class="container">
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Generator: Adobe Illustrator 23.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             viewBox="0 0 1659.8 526.9" style="enable-background:new 0 0 1659.8 526.9;" xml:space="preserve">
+        <style type="text/css">
+            .st0{fill:#139EE9;}
+            .st1{fill:#002BCF;}
+            .st2{fill:#FF9D00;}
+            .st3{fill:#FFEC00;}
+            .st4{enable-background:new    ;}
+        </style>
+        <title>final-21</title>
+        <path class="logo-piece st0" d="M81.5,188.6l-19.7-8.7c35.7-86.5,134.1-150,242.2-133.2c121,18.7,202.6,126.3,192.5,243.8
+            c-10.2,119.2-108.9,207.7-225.9,208.2c-117,0.5-212.6-86.9-226.6-198.8l21.3-2.7c10.1,64.7,41.8,115.1,97.3,149.5
+            c38.4,23.8,80.7,33.6,125.8,29.3c98.8-9.4,172.5-84.6,185.1-176.9c13-95.6-42.2-187.3-132.8-220.5c-66-24.3-139.8-13.2-195.7,29.4
+            C117.1,129.4,96.1,156.3,81.5,188.6z"/>
+        <path class="logo-piece st1" d="M487.8,437.5l-16.9-13c47.3-65.1,63-136.8,43.4-215c-14.6-58.3-47.3-105.2-96-140.4
+            C321.8-0.6,188.5,8.2,101.6,89c-88.2,82-108.5,222-33.3,328.2c-5.8,4.2-11.6,8.3-17.4,12.4c-69.9-93.1-74.3-245.2,27.4-348.3
+            C184.2-26,352.4-25.5,457.8,73.4C567.5,176.3,567.6,336,487.8,437.5z"/>
+        <path class="logo-piece st2" d="M302.9,82.1l-3.6,20.9c-52-7.6-98.6,4.1-139,37.4c-29.9,24.7-49.1,56.3-57.4,94.2
+            c-17.8,81.7,24.8,161.4,99,193.5c74.5,32.2,161.3,7.7,208-58.6c48.7-69.2,39.9-168.9-28-227.4l14-16.2
+            c67.1,56.4,90.4,160.5,39.1,245c-52.8,87-162.8,118.1-252.6,72.2C92.9,397.5,53,291,91.3,198.5C131.5,101.6,230.4,67.3,302.9,82.1z"
+            />
+        <path class="logo-piece st3" d="M190.4,409.5c3.6-6.2,7.1-12.3,10.7-18.5c37.8,20.4,76.9,24.1,117.2,9.2c31-11.4,54.8-32,71.4-60.7
+            c32.3-55.7,20.5-127.2-28.1-171c-48.1-43.3-121.3-46.4-173.8-7.8c-51.8,38.1-73.9,112.7-38.4,177.9l-18.4,10.3
+            c-29.3-46.8-33.1-129.6,24.3-188.6c60.8-62.5,157.7-64.5,220.6-8.3c64.8,57.9,72.2,154.5,17.6,220.5
+            C337.8,439.8,248.1,444.4,190.4,409.5z"/>
+        <g class="st4">
+            <path class="st1" d="M704.3,190.8h-70l-19.4,47.8h-24.2l79.5-189.6h1.5l79.5,189.6h-28.2L704.3,190.8z M695.9,169.7L670,103.2
+                l-26.9,66.5H695.9z"/>
+            <path class="st1" d="M813.6,144c4.9-4.4,10.2-7.7,15.7-10.1c5.5-2.3,10.4-3.6,14.6-3.8l-1,23.4c-7.6-0.3-14.4,1.2-20.5,4.5
+                c-6.1,3.4-10.9,8-14.3,14c-3.4,6-5.2,12.3-5.2,19v47.6h-23.4V134.4h20.9l1.8,24.7C804.9,153.4,808.7,148.3,813.6,144z"/>
+            <path class="st1" d="M907.9,88.6v46.1h28.4v18.6h-28.4v85.3h-23.7v-85.3h-19.1v-18.6h19.1V88.6H907.9z"/>
+        </g>
+        <g class="st4">
+            <path class="st1" d="M608.8,285h49.8c22.7,0,41.4,4.5,56.3,13.5c14.9,9,25.7,20.6,32.5,34.7c6.8,14.2,10.2,29.2,10.2,44.9
+                c0,17.6-4.1,33.2-12.3,46.7c-8.2,13.5-19.2,23.9-33,31.2c-13.8,7.3-28.7,10.9-44.8,10.9h-58.7V285z M662.7,443.4
+                c13.4,0,25.4-2.6,36-7.9c10.6-5.3,18.8-13,24.8-23c6-10.1,8.9-22.1,8.9-36c0-14.3-3.1-26.5-9.3-36.8c-6.2-10.2-14.5-18-24.8-23.3
+                c-10.3-5.3-21.4-7.9-33.4-7.9h-31.7v134.9H662.7z"/>
+            <path class="st1" d="M795.4,316c2.9-2.5,6.1-3.8,9.8-3.8c3.7,0,6.9,1.3,9.7,3.8s4.2,5.6,4.2,9.3c0,3.7-1.4,6.8-4.2,9.3
+                s-6,3.8-9.7,3.8c-3.7,0-7-1.3-9.8-3.8c-2.9-2.5-4.3-5.6-4.3-9.3C791.1,321.6,792.5,318.5,795.4,316z M816.3,362.8V467h-23.4V362.8
+                H816.3z"/>
+            <path class="st1" d="M864.5,465.3c-7-2.7-12.6-6.6-17-11.8l10.1-13.6c4.2,4,8.6,7.1,13.2,9.3c4.6,2.2,9.2,3.3,13.7,3.3
+                c5.5,0,10.1-1.1,13.6-3.3c3.5-2.2,5.3-5.5,5.3-10.1c0-3.5-1.1-6.4-3.3-8.6c-2.2-2.2-4.7-3.8-7.4-4.8c-2.8-1-7.4-2.5-14-4.5
+                c-17.8-5.7-26.7-15.9-26.7-30.5c0-8.7,3.2-16.2,9.7-22.4c6.5-6.2,15.5-9.3,27.1-9.3c7.4,0,13.9,0.9,19.6,2.8
+                c5.7,1.8,11,4.8,15.9,8.8L915,385c-2.7-2.9-6.1-5.1-10.2-6.8c-4.1-1.7-8-2.6-11.7-2.8c-4.7,0-8.7,1.1-12.1,3.4
+                c-3.4,2.3-5,5.3-5,9.2c0,3.2,1.1,5.8,3.3,7.9c2.2,2.1,4.4,3.6,6.8,4.5c2.3,0.9,7,2.5,13.8,4.7c8.6,2.9,15.2,6.5,20,11.1
+                c4.8,4.5,7.2,10.7,7.2,18.4c0,10.1-3.5,18.4-10.6,24.9c-7,6.5-17,9.8-30,9.8C878.8,469.3,871.4,467.9,864.5,465.3z"/>
+            <path class="st1" d="M1027.6,466.3c-6.5,2.2-13.1,3.3-19.8,3.3c-11.2,0-21.2-2.3-29.8-6.8c-8.6-4.5-15.4-10.9-20.1-19.3
+                c-4.8-8.3-7.2-17.9-7.2-28.8c0-10.7,2.5-20.3,7.6-28.6c5-8.3,11.7-14.7,20.1-19.3c8.4-4.5,17.5-6.8,27.2-6.8
+                c15.9,0,29.1,4.4,39.5,13.1l-11.8,15.4c-3.4-2.5-7.2-4.7-11.6-6.4c-4.4-1.8-9.1-2.6-14.1-2.6c-6.2,0-11.9,1.6-17,4.7
+                c-5.1,3.1-9.1,7.3-12.1,12.7c-2.9,5.4-4.4,11.3-4.4,17.9s1.5,12.5,4.7,17.9c3.1,5.4,7.3,9.7,12.7,12.8c5.4,3.2,11.5,4.8,18.4,4.8
+                c8.6,0,16-2.5,22.4-7.6l12.1,14.6C1039.6,461.1,1034.1,464.1,1027.6,466.3z"/>
+        </g>
+        <g class="st4">
+            <path class="st1" d="M1072.9,385.8c4.9-8.5,11.8-15.1,20.6-20c8.8-4.9,18.8-7.3,29.8-7.3s20.9,2.4,29.6,7.3
+                c8.6,4.9,15.4,11.5,20.1,19.9c4.8,8.4,7.2,17.8,7.2,28.2c0,10.4-2.4,19.8-7.2,28.3c-4.8,8.5-11.5,15.1-20.3,20
+                c-8.7,4.9-18.7,7.3-30,7.3c-10.9,0-20.7-2.3-29.5-6.9c-8.7-4.6-15.6-11.1-20.5-19.5c-5-8.4-7.4-18.1-7.4-29.2
+                C1065.5,403.7,1068,394.3,1072.9,385.8z M1093.5,432.3c2.9,5.5,7,9.9,12.1,13.1c5.1,3.2,10.8,4.8,17,4.8c9.7,0,17.8-3.4,24.2-10.3
+                c6.4-6.9,9.6-15.4,9.6-25.7c0-10.2-3.2-18.8-9.6-25.8c-6.4-7-14.4-10.4-24.2-10.4c-6.4,0-12.1,1.6-17.1,4.9c-5,3.3-9,7.7-12,13.2
+                c-2.9,5.5-4.4,11.6-4.4,18.1S1090.6,426.7,1093.5,432.3z"/>
+        </g>
+        <g class="st4">
+            <path class="st1" d="M1307.6,362.8L1252,470h-1.3l-57.9-107.2h27.9l31.5,66.5l29.2-66.5H1307.6z"/>
+        </g>
+        <g class="st4">
+            <path class="st1" d="M1404.6,440.3l10.8,17.4c-5.5,3.7-11.5,6.6-18,8.7c-6.5,2.1-12.7,3.1-18.8,3.1c-11.6,0-21.7-2.4-30.3-7.2
+                c-8.6-4.8-15.3-11.4-20-19.8c-4.7-8.4-7-17.9-7-28.4c0-10.2,2.6-19.6,7.7-28.1c5.1-8.5,12.1-15.2,21-20.1
+                c8.9-4.9,18.6-7.4,29.2-7.4c13.6,0,25,4,34.1,12c9.1,8,15.4,19.1,18.8,33.4l-82.6,28.9c3,5.2,7.1,9.3,12.2,12.2
+                c5.1,2.9,11,4.4,17.5,4.4C1387.9,449.4,1396.3,446.4,1404.6,440.3z M1353.7,387.2c-6.2,6.7-9.3,15.3-9.3,25.7
+                c0,2.5,0.1,4.4,0.3,5.8l60.7-22.4c-2.5-5.7-6.1-10.3-10.7-13.8c-4.6-3.5-10.4-5.3-17.2-5.3C1367.8,377.2,1359.9,380.5,1353.7,387.2
+                z"/>
+            <path class="st1" d="M1495.3,372.4c4.9-4.4,10.2-7.7,15.7-10.1c5.5-2.3,10.4-3.6,14.6-3.8l-1,23.4c-7.6-0.3-14.4,1.2-20.5,4.5
+                c-6.1,3.4-10.9,8-14.3,14c-3.4,6-5.2,12.3-5.2,19V467h-23.4V362.8h20.9l1.8,24.7C1486.5,381.8,1490.4,376.7,1495.3,372.4z"/>
+            <path class="st1" d="M1655,362.8l-67.2,154.3h-23.2l23.9-53.6l-48.3-100.7h26.4l33.7,74.3l33-74.3H1655z"/>
+        </g>
+        </svg>
+        
+      {{-- <div class="logo-pieces">
               <img class="logo-piece" src="{{ asset('img/logo-1.png') }}">
-              <img class="logo-piece" src="{{ asset('img/logo-2.png') }}">
-              <img class="logo-piece" src="{{ asset('img/logo-3.png') }}">
-              <img class="logo-piece" src="{{ asset('img/logo-4.png') }}">
-              <img class="text-logo" src="{{ asset('img/logo.png') }}">
-            </div>
+      <img class="logo-piece" src="{{ asset('img/logo-2.png') }}">
+      <img class="logo-piece" src="{{ asset('img/logo-3.png') }}">
+      <img class="logo-piece" src="{{ asset('img/logo-4.png') }}">
+      <img class="text-logo" src="{{ asset('img/logo.png') }}">
+  </div> --}}
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-globe-americas mr-1"></i>@lang('home.language')
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-globe-americas mr-1"></i>@lang('home.language')
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/lang/hy"><span
-                                class="flag-icon flag-icon-am mr-2"></span>Հայերեն</a>
-                        <a class="dropdown-item" href="/lang/en"><span
-                                class="flag-icon flag-icon-gb mr-2"></span>English</a>
-                        <a class="dropdown-item" href="/lang/ru"><span
-                                class="flag-icon flag-icon-ru mr-2"></span>Русский</a>
-                    </div>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">@lang('home.login')</a>
-                </li>
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">@lang('home.register')</a>
-                </li>
-                @endif
-                @else
-                @if(Auth::user()->type == "admin")
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('cpanel') }}"><i
-                            class="fas fa-user-cog mr-1"></i>@lang('home.cpanel')</a>
-                </li>
-                @endif
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        <i class="fas fa-user mr-1"></i>{{ Auth::user()->first_name }} <span class="caret"></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('users.show', Auth::user()->id) }}">
-                            <i class="fas fa-user-circle mr-1"></i>@lang('home.profile')
-                        </a>
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                           document.getElementById('logout-form').submit();">
-                            <i class="fas fa-sign-out-alt mr-1"></i>@lang('home.logout')
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-                @endguest
-            </ul>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/lang/hy"><span class="flag-icon flag-icon-am mr-2"></span>Հայերեն</a>
+          <a class="dropdown-item" href="/lang/en"><span class="flag-icon flag-icon-gb mr-2"></span>English</a>
+          <a class="dropdown-item" href="/lang/ru"><span class="flag-icon flag-icon-ru mr-2"></span>Русский</a>
         </div>
-    </div>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      @guest
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">@lang('home.login')</a>
+      </li>
+      @if (Route::has('register'))
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">@lang('home.register')</a>
+      </li>
+      @endif
+      @else
+      @if(Auth::user()->type == "admin")
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('cpanel') }}"><i class="fas fa-user-cog mr-1"></i>@lang('home.cpanel')</a>
+      </li>
+      @endif
+      <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false" v-pre>
+          <i class="fas fa-user mr-1"></i>{{ Auth::user()->first_name }} <span class="caret"></span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="{{ route('users.show', Auth::user()->id) }}">
+            <i class="fas fa-user-circle mr-1"></i>@lang('home.profile')
+          </a>
+          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+            <i class="fas fa-sign-out-alt mr-1"></i>@lang('home.logout')
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
+        </div>
+      </li>
+      @endguest
+    </ul>
+  </div>
+  </div>
 </nav>
