05c12e346004c4eae0c14d912a5fb1388e8e42c2
Areg <areg.areg51@gmail.com>
2019-09-24 01:06:18 +0400

    logo fix

--- public/js/app.js
+++ public/js/app.js
@@ -37012,8 +37012,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\artdiscovery\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\artdiscovery\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\artdiscovery\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\artdiscovery\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
