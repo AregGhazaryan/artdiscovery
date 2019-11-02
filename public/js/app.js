3de00b9872d22a9cdb118df44761e3fa402a2133
Areg <areg.areg51@gmail.com>
2019-11-02 19:19:50 +0400

    v1.424pa

--- public/js/app.js
+++ public/js/app.js
@@ -70567,6 +70567,7 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   cluster: "mt1",
   wsHost: window.location.hostname,
   wsPort: 6001,
+  enabledTransports: ['ws', 'wss'],
   encrypted: true
 });
 
