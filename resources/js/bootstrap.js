1812291e5c86d9f33ee44c72f26957d031777b71
Areg <areg.areg51@gmail.com>
2019-11-09 15:44:59 +0400

    v1.516pa

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -53,8 +53,7 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
-    // enabledTransports: 'ws',
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: 'ws',
+    // enabledTransports: ['ws', 'wss'],
     encrypted: true
 });
