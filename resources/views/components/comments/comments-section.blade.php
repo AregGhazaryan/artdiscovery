12327fdd0749c9fc2eff42bfac688075f455279a
Areg <areg.areg51@gmail.com>
2019-11-18 00:25:46 +0400

    v1.857pa

--- resources/views/components/comments/comments-section.blade.php
+++ resources/views/components/comments/comments-section.blade.php
@@ -2,7 +2,7 @@
 @auth
     <div class="input-group">
       <input type="text" name="comment_body" class="form-control" />
-      <button data-post="{{ $post->id }}" type="button" style="background-color:#{{ $post->section->color }};" class="btn add-comment-button submit-comment">
+      <button data-post="{{ $post->id }}" type="button" style="background-color:#{{ $post->section->color }};" class="text-light btn add-comment-button submit-comment">
       @lang('comments.add')</button>
     </div>
 
